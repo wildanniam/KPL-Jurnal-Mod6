caff4e7a5286a268703f64caf3c5628d89a2a537
wildanniam <wildanniam4@gmail.com>
2024-03-25 06:44:19 +0700

    init project

---
+++ jurnal_mod6/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace jurnal_mod6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
